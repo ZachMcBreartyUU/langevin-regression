8706816f5be00e22109e2ea83e78771fa62dbc60
Zachery McBrearty <z.g.mcbrearty@uu.nl>
2025-09-18 15:11:19 +0200

    rename so _send from send_ to group files correct plot intermediate apply to runs without constant sigma

--- softglass_logP_send.py
+++ softglass_logP_send.py
@@ -4,10 +4,14 @@ import subprocess
 EQN_LEARN_PATH = "runs_EqnLearning/"
 
 for foldername in [
-    "run_0D_0D_5_1_const_3",
-    "run_0D_0D_5_1_const_4",
-    "run_0D_0D_5_1_const_35",
-    "run_0D_0D_5_1_const_37",
+    # "run_0D_0D_5_1_const_3",
+    # "run_0D_0D_5_1_const_4",
+    # "run_0D_0D_5_1_const_35",
+    # "run_0D_0D_5_1_const_37",
+    "run_0D_0D_5_1_000",
+    "run_0D_0D_5_1_004",
+    "run_0D_0D_6_1_000",
+    "run_0D_0D_6_1_004",
 ]:
     folder = EQN_LEARN_PATH + foldername
     subprocess.run(
@@ -21,5 +25,6 @@ for foldername in [
             folder,
             "--kl-reg",
             "0",
+            "--plot-intermediate",
         ]
     )
