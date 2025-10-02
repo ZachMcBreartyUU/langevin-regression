800520716c75a5879407b1a38dbd090024e4d669
Zachery McBrearty <z.g.mcbrearty@uu.nl>
2025-10-02 10:29:43 +0200

    place out and err in subfolder

--- process_data_send.py
+++ process_data_send.py
@@ -9,18 +9,18 @@ for foldername in [
     # "run_0D_0D_5_1_const_35",
     # "run_0D_0D_5_1_const_37",
     "run_0D_0D_5_1_000",
-    "run_0D_0D_5_1_004",
-    "run_0D_0D_6_1_000",
-    "run_0D_0D_6_1_004",
+    # "run_0D_0D_5_1_004",
+    # "run_0D_0D_6_1_000",
+    # "run_0D_0D_6_1_004",
 ]:
     folder = EQN_LEARN_PATH + foldername
     subprocess.run(
         [
             "sbatch",
             "-o",
-            f"/scratch/seismology/zach/{folder}_process.out",
+            f"/scratch/seismology/zach/{folder}/process_0D.out",
             "-e",
-            f"/scratch/seismology/zach/{folder}_process.err",
+            f"/scratch/seismology/zach/{folder}/process_0D.err",
             "/eejit/home/zach/LearningSoftglassEqns/Langevin_regression_method/langevin-regression/process_data.slurm",
             folder,
             "-N",
