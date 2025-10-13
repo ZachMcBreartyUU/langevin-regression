c643d6242f325225bf2e66186344cde0bdde1a67
Zachery McBrearty <z.g.mcbrearty@uu.nl>
2025-10-13 17:51:34 +0200

    files for the regression of a model with fixed parameters in order to evaluate how the model works against different inputs

---
+++ DIFFERENTMODELS/fixed_model.py
@@ -0,0 +1,359 @@
+from pathlib import Path
+from typing import Optional
+from time import time
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.axisartist import Axes  # typing
+from numpy.linalg import lstsq
+
+# sindy libraries
+import sympy
+
+# data generation and final model integration
+import symengine
+from jitcsde import jitcsde, y
+
+from kramersmoyal import km
+
+from utils import (
+    cost_KL,
+    optimise_function,
+    sindy_model,
+    SteadyFP,
+)
+
+SCRATCH_PATH = Path(f"/scratch/seismology/zach/softglass/fixed_model")
+# SCRATCH_PATH.mkdir(parents=True, exist_ok=True)
+
+
+def run_fixed_model(
+    EVEN_ABS=True,
+    PDF_WEIGHTS=False,
+    dt=0.001,
+    num_datapoints=10_000_000,
+    num_bins=100,
+    kl_reg=0.001,
+    ep0=0.2,
+    ep1=0.1,
+    coeffs=[0.0, -1.0, 0.0, 1.0, -1 / 3],
+    x0=0.0,
+    seed: Optional[int] = None,
+):
+    times = np.arange(0, num_datapoints) * dt
+
+    ## Define model: dx = A(x)dt + B(x)dw; C(x) = B^2 / 2
+    # Higher order Pitchfork with multiplicative noise
+    x = y(0)
+    A = 0
+    if EVEN_ABS:
+        for i in range(len(coeffs)):
+            if i % 2 == 0 and i != 0:
+                A += x ** (i - 1) * symengine.Abs(x) * coeffs[i]
+            else:
+                A += x**i * coeffs[i]
+    else:
+        for i in range(len(coeffs)):
+            A += x**i * coeffs[i]
+    A = [A]
+    B = [symengine.sqrt(ep0 + ep1 * x**2)]
+
+    ## Integrate model
+    SDE = jitcsde(A, B, n=1, additive=False, verbose=False)
+    SDE.set_initial_value([x0])
+    SDE.set_seed(seed)
+    x_data = np.fromiter(
+        (SDE.integrate(t)[0] for t in times), dtype=float, count=num_datapoints
+    )
+    assert x_data.shape == (num_datapoints,)
+
+    ## Perform Kramers Moyal
+    edges = np.linspace(np.min(x_data), np.max(x_data), num_bins + 1)
+    # edges = np.linspace(-0.005, 0.005, num_bins + 1)
+    kmc, centers = km(x_data[..., None], bins=(edges,), powers=2)  # type: ignore
+    pdf, moment_1, moment_2 = kmc
+    centers_x = centers[0]
+    pdf /= np.nansum(pdf)
+    moment_1 /= dt
+    moment_2 /= dt
+    del x_data
+
+    ## Make SINDy libraries
+    x_sym = sympy.symbols("x")
+
+    if EVEN_ABS:
+        arr = []
+        for i in range(len(coeffs)):
+            if i % 2 == 0 and i != 0:
+                arr.append(x_sym ** (i - 1) * symengine.Abs(x_sym))
+            else:
+                arr.append(x_sym**i)
+        A_lib_expr = np.array(arr)
+    else:
+        A_lib_expr = np.array([x_sym**i for i in range(len(coeffs) + 3)])
+    # print(f"BEFORE FILTER {A_lib_expr=}")
+    # ONLY REGRESS ON THE CORRECT TERMS
+    A_lib_expr = A_lib_expr[np.nonzero(coeffs)]
+    # print(f"AFTER FILTER {A_lib_expr=}")
+    num_A_expr = len(A_lib_expr)
+
+    lib_A = np.empty((num_A_expr, num_bins))
+    for k in range(num_A_expr):
+        lamb_expr = sympy.lambdify(x_sym, A_lib_expr[k])
+        lib_A[k] = lamb_expr(centers_x)
+
+    C_lib_expr = np.array([x_sym**0, x_sym**2])
+    # print(f"{C_lib_expr=}")
+    num_C_expr = len(C_lib_expr)
+
+    lib_C = np.empty((num_C_expr, num_bins))
+    for k in range(num_C_expr):
+        lamb_expr = sympy.lambdify(x_sym, C_lib_expr[k])
+        lib_C[k] = lamb_expr(centers_x)
+
+    n_terms = num_A_expr + num_C_expr
+
+    # Regress on fixed terms
+    Xi0 = np.empty((num_A_expr + num_C_expr))
+    Xi0[:num_A_expr] = lstsq(lib_A.T, moment_1)[0]
+    Xi0[num_A_expr:] = lstsq(lib_C.T, moment_2)[0]
+    # print(f"{Xi0=}")
+    if PDF_WEIGHTS:
+        weight = pdf
+    else:
+        weight = np.ones_like(pdf)
+    weights = np.array([weight, weight])
+
+    sfp = SteadyFP(num_bins, centers_x[1] - centers_x[0])
+
+    params = {
+        "W": weights,
+        "Xi0": Xi0,
+        "A_KM": moment_1,
+        "C_KM": moment_2,
+        "A_expr": A_lib_expr,
+        "C_expr": C_lib_expr,
+        "lib_A": lib_A,
+        "lib_C": lib_C,
+        "sfp": sfp,
+        "pdf": pdf,
+        "kl_reg": kl_reg,
+    }
+
+    # opt_func = lambda params: optimise_function(cost_KL, params)
+    xi, cost_val = optimise_function(cost_KL, params)
+
+    # TODO: Could perform some rounding on the found params, e.g. 0.012324 -> 0.012
+    A_sym = sindy_model(xi[:num_A_expr], A_lib_expr)
+    A_sindy = sympy.lambdify(x_sym, A_sym)(centers_x)
+    C_sym = sindy_model(xi[num_A_expr:], C_lib_expr)
+    C_sindy = sympy.lambdify(x_sym, C_sym)(centers_x)
+
+    if np.ndim(A_sindy) == 0:
+        A_sindy = np.full_like(centers_x, A_sindy)
+    if np.ndim(C_sindy) == 0:
+        C_sindy = np.full_like(centers_x, C_sindy)
+
+    # print(f"dx = ({A_sym}) dt + ({sympy.sqrt(2.0*C_sym)}) dβ")
+
+    ## Directly compare True answer to Found answer
+    true_model_xi = np.zeros(n_terms)
+    true_model_xi[:num_A_expr] = np.array(coeffs)[np.nonzero(coeffs)]
+    true_model_xi[num_A_expr + 0] = ep0
+    true_model_xi[num_A_expr + 1] = ep1
+
+    found_pdf = sfp.solve(A_sindy, C_sindy)
+
+    differences = np.abs(xi - true_model_xi)
+    return xi, cost_val, differences, centers_x, pdf, found_pdf
+
+
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+seed = 987654321
+start = time()
+
+
+# TODO: implement repetition?
+def do_test(
+    target_var,
+    range_var,
+    logx=True,
+    coeff_labels=["x", "x^3", "x^3|x|"],
+    ONLYPLOT=5,
+):
+    start = time()
+    print(f"{target_var} started", flush=True)
+
+    costs = []
+    diffs = []
+    centers = []
+    pdfs = []
+    found_pdfs = []
+    for var in range_var:
+        print(f"{target_var}={var}, started at {time() - start}", flush=True)
+        xi, cost, diff, center, pdf, found_pdf = run_fixed_model(
+            **{target_var: var}, seed=seed
+        )
+        costs.append(cost)
+        diffs.append(diff)
+        centers.append(center)
+        pdfs.append(pdf)
+        found_pdfs.append(found_pdf)
+    diffs = np.array(diffs).T
+
+    fig_cost, ax_cost = plt.subplots()
+    fig_diff, axes_diff = plt.subplots(len(diffs))
+    fig_pdfs, ax_pdfs = plt.subplots()
+    fig_found_pdfs, ax_found_pdfs = plt.subplots()
+    if logx:
+        ax_cost.semilogx(range_var, costs)
+        for diff, ax_diff in zip(diffs, axes_diff):
+            ax_diff.semilogx(range_var, diff)
+    else:
+        ax_cost.plot(range_var, costs)
+        for diff, ax_diff in zip(diffs, axes_diff):
+            ax_diff.plot(range_var, diff)
+    ax_cost.set_xlabel(f"{target_var}")
+    ax_cost.set_ylabel("Cost / misfit, V")
+    for ax_diff, coeff_lab in zip(axes_diff, coeff_labels):
+        ax_diff.set_xlabel(f"{target_var}")
+        ax_diff.set_ylabel(
+            rf"Absolute difference, $\left|\Delta\xi_{coeff_lab}\right|$"
+        )
+    skip = int(len(pdfs) // ONLYPLOT)
+    slce = slice(skip // 2, None, skip)
+    for center, pdf, var in zip(centers[slce], pdfs[slce], range_var[slce]):
+        ax_pdfs.plot(center, pdf, label=f"{target_var}={var}")
+    ax_pdfs.set_xlabel("x")
+    ax_pdfs.set_ylabel("pdf(x)")
+    ax_pdfs.legend()
+
+    skip = int(len(found_pdfs) // ONLYPLOT)
+    for center, found_pdf, var in zip(
+        centers[slce],
+        found_pdfs[slce],
+        range_var[slce],
+    ):
+        ax_found_pdfs.plot(center, found_pdf, label=f"{target_var}={var}")
+    ax_found_pdfs.set_xlabel("x")
+    ax_found_pdfs.set_ylabel("pdf(x)")
+    ax_found_pdfs.legend()
+
+    fig_cost.tight_layout()
+    fig_diff.tight_layout()
+    fig_pdfs.tight_layout()
+    fig_found_pdfs.tight_layout()
+    fig_cost.savefig(SCRATCH_PATH / f"cost_vs_{target_var}.png")
+    fig_diff.savefig(SCRATCH_PATH / f"diff_vs_{target_var}.png")
+    fig_pdfs.savefig(SCRATCH_PATH / f"pdf_vs_{target_var}.png")
+    fig_found_pdfs.savefig(SCRATCH_PATH / f"found_pdf_vs_{target_var}.png")
+
+    print(f"{target_var}, finished at {time() - start}", flush=True)
+
+
+def do_test_coeffs(
+    target_name,
+    target_index,
+    range_var,
+    default_coeffs=[0.0, -1.0, 0.0, 1.0, -1 / 3],
+    logx=True,
+    coeff_labels=["x", "x^3", "x^3|x|"],
+    ONLYPLOT=5,
+):
+
+    start = time()
+    print(f"{target_name} started", flush=True)
+    costs = []
+    diffs = []
+    centers = []
+    pdfs = []
+    found_pdfs = []
+    for var in range_var:
+        print(f"{target_name}={var}, started at {time() - start}", flush=True)
+        default_coeffs[target_index] = var
+        xi, cost, diff, center, pdf, found_pdf = run_fixed_model(
+            coeffs=default_coeffs, seed=seed
+        )
+        costs.append(cost)
+        diffs.append(diff)
+        centers.append(center)
+        pdfs.append(pdf)
+        found_pdfs.append(found_pdf)
+    diffs = np.array(diffs).T
+
+    fig_cost, ax_cost = plt.subplots()
+    fig_diff, axes_diff = plt.subplots(len(diffs))
+    fig_pdfs, ax_pdfs = plt.subplots()
+    fig_found_pdfs, ax_found_pdfs = plt.subplots()
+    if logx:
+        ax_cost.semilogx(range_var, costs)
+        for diff, ax_diff in zip(diffs, axes_diff):
+            ax_diff.semilogx(range_var, diff)
+    else:
+        ax_cost.plot(range_var, costs)
+        for diff, ax_diff in zip(diffs, axes_diff):
+            ax_diff.plot(range_var, diff)
+    ax_cost.set_xlabel(f"{target_name}")
+    ax_cost.set_ylabel("Cost / misfit, V")
+    for ax_diff, coeff_lab in zip(axes_diff, coeff_labels):
+        ax_diff.set_xlabel(f"{target_name}")
+        ax_diff.set_ylabel(
+            rf"Absolute difference, $\left|\Delta\xi_{coeff_lab}\right|$"
+        )
+
+    skip = int(len(pdfs) // ONLYPLOT)
+    slce = slice(skip // 2, None, skip)
+    for center, pdf, var in zip(centers[slce], pdfs[slce], range_var[slce]):
+        ax_pdfs.plot(center, pdf, label=f"{target_name}={var}")
+    ax_pdfs.set_xlabel("x")
+    ax_pdfs.set_ylabel("pdf(x)")
+    ax_pdfs.legend()
+
+    for center, found_pdf, var in zip(
+        centers[slce],
+        found_pdfs[slce],
+        range_var[slce],
+    ):
+        ax_found_pdfs.plot(center, found_pdf, label=f"{target_name}={var}")
+    ax_found_pdfs.set_xlabel("x")
+    ax_found_pdfs.set_ylabel("pdf(x)")
+    ax_found_pdfs.legend()
+
+    fig_cost.tight_layout()
+    fig_diff.tight_layout()
+    fig_pdfs.tight_layout()
+    fig_found_pdfs.tight_layout()
+    fig_cost.savefig(SCRATCH_PATH / f"cost_vs_{target_name}.png")
+    fig_diff.savefig(SCRATCH_PATH / f"diff_vs_{target_name}.png")
+    fig_pdfs.savefig(SCRATCH_PATH / f"pdf_vs_{target_name}.png")
+    fig_found_pdfs.savefig(SCRATCH_PATH / f"found_pdf_vs_{target_name}.png")
+
+    print(f"{target_name}, finished at {time() - start}", flush=True)
+
+
+### program splits
+if rank == (0 % size):
+    do_test("num_datapoints", np.logspace(5, 8, 20).astype(int))
+elif rank == (1 % size):
+    do_test("kl_reg", np.logspace(-6, 0, 20))
+elif rank == (2 % size):
+    do_test("dt", np.logspace(-5, -1.5, 10))
+elif rank == (3 % size):
+    do_test("num_bins", np.logspace(1, 3, 20).astype(int))
+elif rank == (4 % size):
+    do_test("ep0", np.logspace(-7, 0, 20))
+elif rank == (5 % size):
+    do_test("ep1", np.logspace(-7, 0, 20))
+elif rank == (6 % size):
+    do_test_coeffs("ax", 1, np.linspace(-3, 3, 20), logx=False)
+elif rank == (7 % size):
+    do_test_coeffs("bx3", 3, np.linspace(-3, 3, 20), logx=False)
+elif rank == (8 % size):
+    do_test_coeffs("cx4", 4, np.linspace(-1, -0.01, 20, endpoint=False), logx=False)
+else:
+    print("Over extended:", rank, size, flush=True)
