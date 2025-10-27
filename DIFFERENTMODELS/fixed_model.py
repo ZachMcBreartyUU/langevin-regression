59600def63c3f963534d9414f607b7f4c5af2c92
Zachery McBrearty <z.g.mcbrearty@uu.nl>
2025-10-27 16:53:32 +0100

    set the correct xi value to always be negative

--- DIFFERENTMODELS/fixed_model.py
+++ DIFFERENTMODELS/fixed_model.py
@@ -118,8 +118,7 @@ def run_fixed_model(
     Xi0[:num_A_expr] = lstsq(lib_A.T, moment_1)[0]
     Xi0[num_A_expr:] = lstsq(lib_C.T, moment_2)[0]
 
-    if Xi0[-1] > 0:
-        Xi0[-1] *= -1
+    Xi0[num_A_expr] = -np.abs(Xi0[num_A_expr])
     # print(f"{Xi0=}")
     if PDF_WEIGHTS:
         weight = pdf
