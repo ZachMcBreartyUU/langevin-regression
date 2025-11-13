07fdea9547190115cdf841f6590dce4ff0c94e78
Zachery McBrearty <z.g.mcbrearty@uu.nl>
2025-11-13 11:36:52 +0100

    f / a to A / C track which indices are found valid to be removed remove the correct index from the model by looking at valid indices

--- DIFFERENTMODELS/utils_parallel.py
+++ DIFFERENTMODELS/utils_parallel.py
@@ -29,29 +29,37 @@ def SSR_loop_parallel(opt_fun, params):
     for k in range(1, n_terms - 1):
         start = time()
         params_list = []
+        valid_indices = []
         for j in range(len(active)):
             tmp_active = active.copy()
             tmp_active = np.delete(tmp_active, j)
 
             reduced_params = params.copy()
-            f_active = tmp_active[tmp_active < A_len]
-            a_active = tmp_active[tmp_active >= A_len] - A_len
-            reduced_params["A_expr"] = params["A_expr"][f_active]
-            reduced_params["C_expr"] = params["C_expr"][a_active]
-            reduced_params["lib_A"] = params["lib_A"][f_active]
-            reduced_params["lib_C"] = params["lib_C"][a_active]
-            reduced_params["Xi0"] = params["Xi0"][tmp_active]
+            A_active = tmp_active[tmp_active < A_len]
+            C_active = tmp_active[tmp_active >= A_len] - A_len
+            if len(A_active) == 0 or len(C_active) == 0:
+                continue
+
+            reduced_params["A_expr"] = params["A_expr"][A_active]
+            reduced_params["C_expr"] = params["C_expr"][C_active]
+            reduced_params["lib_A"] = params["lib_A"][A_active]
+            reduced_params["lib_C"] = params["lib_C"][C_active]
+            Xi_copy = params["Xi0"]
+            Xi_copy[A_active[-1]] = -abs(Xi_copy[A_active[-1]])
+            reduced_params["Xi0"] = Xi_copy[tmp_active]
             params_list.append(reduced_params)
+            valid_indices.append(j)
         with mp.Pool(NUM_CPUS) as p:
             results = p.map(opt_fun, params_list)
-        Vs = []
         Xis = []
+        Vs = []
         for Xi, V in results:
             Xis.append(Xi)
             Vs.append(V)
-        min_idx = np.argmin(Vs)
-        min_V = Vs[min_idx]
-        min_Xi = Xis[min_idx]
+        min_found = np.nanargmin(Vs)
+        min_idx = valid_indices[min_found]
+        min_V = Vs[min_found]
+        min_Xi = Xis[min_found]
         active = np.delete(active, min_idx)
         minimum_Vs[k] = min_V
         minimum_Xis[k, active] = min_Xi
@@ -60,10 +68,10 @@ def SSR_loop_parallel(opt_fun, params):
         print(f"Regression {k} took {end-start} seconds")
         print(f"Cost = {min_V} from Xi = {min_Xi}")
 
-        f_active = active[active < A_len]
-        a_active = active[active >= A_len] - A_len
-        print(f"Active f: {params["A_expr"][f_active]}")
-        print(f"Active a: {params["C_expr"][a_active]}", flush=True)
+        A_active = active[active < A_len]
+        C_active = active[active >= A_len] - A_len
+        print(f"Active A: {params["A_expr"][A_active]}")
+        print(f"Active C: {params["C_expr"][C_active]}\n", flush=True)
 
         params["Xi0"][active] = min_Xi
 
