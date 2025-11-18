6f677a4a575fcd980c9b46329d074d163a877184
Zachery McBrearty <z.g.mcbrearty@uu.nl>
2025-11-18 11:02:37 +0100

    swap the cost_just_jef_stack and cost_alpha_stack orders, no longer normalise cost alpha by the data (i.e. div by A_km, C_km)

--- DIFFERENTMODELS/utils_stack.py
+++ DIFFERENTMODELS/utils_stack.py
@@ -130,37 +130,6 @@ def cost_jef_stack(Xi, params):
     return V * (1 - kl_reg) + jef_div * kl_reg
 
 
-def cost_alpha_stack(alpha, params):
-    # Unpack parameters
-    Ws = params["Ws"]  # Optimization weights
-
-    # Kramers-Moyal coefficients
-    A_KMs = params["A_KMs"]
-    C_KMs = params["C_KMs"]
-
-    lib_A = params["lib_A"]
-    lib_C = params["lib_C"]
-
-    Xi = params["Xi0"] * alpha
-
-    # Construct parameterized drift and diffusion functions from libraries and current coefficients
-    A_coeff = Xi[: lib_A.shape[0]]
-    # Penalise an equation with non-negative coefficient on largest term
-    if A_coeff[np.nonzero(A_coeff)[0][-1]] > 0:
-        return np.inf
-    A_vals = lib_A.T @ A_coeff
-    C_vals = lib_C.T @ Xi[lib_A.shape[0] :]
-
-    # Histogram points without data have NaN values in K-M average - ignore these in the average
-    V = np.nansum(
-        Ws[0] * np.abs((A_vals - A_KMs) / A_KMs) ** 2
-        + Ws[1] * np.abs((C_vals - C_KMs) / C_KMs) ** 2
-    )
-    V /= len(A_vals)  # Norm based on number of bins?
-
-    return V
-
-
 def cost_just_jef_stack(Xi, params):
     lib_A = params["lib_A"]
     lib_C = params["lib_C"]
@@ -179,3 +148,33 @@ def cost_just_jef_stack(Xi, params):
     jef_div = np.sum(jef_divs[jef_divs > 0], initial=0.0)
 
     return jef_div
+
+
+def cost_alpha_stack(alpha, params):
+    if alpha < 0:
+        return np.inf
+    # Unpack parameters
+    Ws = params["Ws"]  # Optimization weights
+
+    # Kramers-Moyal coefficients
+    A_KMs = params["A_KMs"]
+    C_KMs = params["C_KMs"]
+
+    lib_A = params["lib_A"]
+    lib_C = params["lib_C"]
+
+    Xi = params["Xi0"] * alpha
+
+    # Construct parameterized drift and diffusion functions from libraries and current coefficients
+    A_coeff = Xi[: lib_A.shape[0]]
+    A_vals = lib_A.T @ A_coeff
+    C_vals = lib_C.T @ Xi[lib_A.shape[0] :]
+
+    # A_KMs[A_KMs == 0] = np.nan
+    # C_KMs[C_KMs == 0] = np.nan
+
+    # Histogram points without data have NaN values in K-M average - ignore these in the average
+    V = np.nansum(Ws[0] * (A_vals - A_KMs) ** 2 + Ws[1] * (C_vals - C_KMs) ** 2)
+    V /= len(A_vals)  # Norm based on number of bins?
+
+    return V
